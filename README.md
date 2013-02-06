a91d0f9e39ad291238443fc6659e0683a44c349d
Ryan Gerry <ryan.gerry@aonhewitt.com>
2013-02-05 22:04:03 -0600

    Initial files.

---
+++ README.md
@@ -0,0 +1,3 @@
+I am reading through the book _The Little Schemer_ and am working through the problems using mit-scheme.
+
+For now, I am running the programs by just cating them to mit-scheme directly.  There is probably a better way to do this.
---
+++ firsts.scm
@@ -0,0 +1,12 @@
+(define firsts
+  (lambda (list)
+    (cond
+      ((null? list)(quote ()))
+      (else (cons (car (car list))
+                  (firsts (cdr list)))))))
+
+(write "the empty list")
+(write (firsts ()))
+
+(write "three element list")
+(write (firsts (quote ((a b) (c d) (e f)))))
