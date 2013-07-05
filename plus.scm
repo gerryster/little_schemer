55de3ce7e6f33f5c2a63b17afb24fb44942c9662
Ryan Gerry <gerryster@gmail.com>
2013-07-05 00:15:02 -0500

    Look ma; unit tests!  Also implemented plus.
    This brings in Alexy Radul's Scheme unit testing framework, test-manager:
    http://web.mit.edu/~axch/www/test_manager.html

--- plus.scm
+++ plus.scm
@@ -1,19 +1,41 @@
-(load "deq?")
+(load "vendor/test-manager/load")
 
 (define add1
   (lambda (n)
     (+ n 1)))
 
-;(define plus
-;  (lambda (a b)
-;))
-
-(write "add1")
-(write (add1 0))
-;(deq? '(2) ((add1 0)))
-(write "done with the deq? check")
+(define sub1
+  (lambda (n)
+    (- n 1)))
 
 
-;(write "three element list")
-;(write (firsts (quote ((a b) (c d) (e f)))))
+(define plus
+  (lambda (a b)
+    (cond
+      ((zero? b) a)
+      (else (plus (add1 a) (sub1 b)))
+    )
+))
 
+(in-test-group
+  plus-and-friends
+
+  (define-test (add1)
+    (check (= 1 (add1 0))  "0++")
+  )
+
+  (define-test (sub1)
+    (check (= -1 (sub1 0))  "0--")
+  )
+
+  (define-test (plus)
+    (check (= 2 (plus 1 1))  "1 + 1")
+    (check (= 1 (plus 1 0))  "1 + 0")
+    (check (= 6 (plus 1 5))  "1 + 5")
+    (check (= 200 (plus 100 100))  "100 + 100")
+    ; TODO: make this work
+    ;(check (= 0 (plus 1 -1))  "1 + -1")
+  )
+)
+
+(run-test '(plus-and-friends))
